692d95e4640e0bb1934160c62e705b22c88fb3aa
rasulkurbanov <rasuljohn139@gmail.com>
2020-12-22 23:59:35 +0500

    Bug fixed on mongoose connection

---
+++ src/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config({ path: './config/config.env' })
+
+
+const connectDB = async () => {
+  try{
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true
+    })
+    console.log(`Successfully connected to MongoDB via mongoose`)
+  }
+  catch(err) {
+    console.log(err)
+  }
+}
+
+
+module.exports = connectDB
