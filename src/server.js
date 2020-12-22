4a4d179eec1ee2f8853a8729f73f68e80b05829a
rasulkurbanov <rasuljohn139@gmail.com>
2020-12-22 23:55:52 +0500

    Added mongoose

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@ const app = express()
 const dotenv = require('dotenv')
 const PORT = process.env.PORT || 5500
 const router = require('./routes/books')
+const connectDB = require('./config/db')
 
 dotenv.config({ path: './config/config.env' })
 
+//Connect to DB
+connectDB()
 
 app.use('/api/books', router)
 
