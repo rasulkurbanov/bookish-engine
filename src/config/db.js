92dfba0a384d8c47f49d505de935c3e1d1273256
rasulkurbanov <rasuljohn139@gmail.com>
2020-12-28 02:00:27 +0500

    Added new features to deleteBook controller

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,6 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: true,
       useUnifiedTopology: true
     })
     console.log(`Successfully connected to MongoDB via mongoose`)
--- src/controllers/books.js
+++ src/controllers/books.js
@@ -4,7 +4,7 @@ const Books = require('../models/Books')
 //@desc GET all books
 //@route GET api/books
 //access PUBLIC
-exports.getBooks = (req, res, next) => {
+exports.getBooks = async (req, res, next) => {
   try {
     const books = await Books.find()
 
@@ -58,14 +58,19 @@ exports.createBook = async (req, res, next) => {
 //@desc Update a single book with an id given in params
 //@route PUT api/books/:id
 //access PUBLIC
-exports.updateBook = (req, res, next) => {
+exports.updateBook = async (req, res, next) => {
+
+  //Declaring req.params.id into id
+  let id = req.params.id
 
   try {
-    const book = await Books.findByIdAndUpdate(req.params.id)
+    const book = await Books.findByIdAndUpdate(id, req.body, {
+      new: true,
+    })
 
     res
       .status(200)
-      .json({ success: true, msg: `Updated a book with an id ${req.params.id}` })
+      .json({ success: true, msg: `Updated a book with an id ${id}` })
 
   }
   catch (err) {
@@ -81,7 +86,24 @@ exports.updateBook = (req, res, next) => {
 //@desc DELETE a single book 
 //@route DELETE api/books/:id
 //access PUBLIC
-exports.deleteBook = (req, res, next) => {
-  res.json({ success: true, msg: `Deleted book with an id ${req.params.id}` })
+exports.deleteBook = async (req, res, next) => {
+
+  //Declaring req.params.id into id
+  let id = req.params.id
+
+  try {
+    await Books.findByIdAndDelete(id)
+
+    res 
+      .status(200)
+      .json({success: true, msg: `Book has been deleted with an id of ${id}`})
+  }
+  catch(err) {
+    res
+      .status(400)
+      .json({ success: false, msg: `Something went wrong with deleting or ${err.message}` }) 
+
+    console.log(err)   
+  }
 
 }
