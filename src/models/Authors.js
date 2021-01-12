faf548a6ed549f9095ffae14e0822e584f82ccbf
rasulkurbanov <rasuljohn139@gmail.com>
2021-01-12 21:58:48 +0500

    Add features to Authors model

--- src/models/Authors.js
+++ src/models/Authors.js
@@ -5,5 +5,25 @@ const Authors = new mongoose.Schema({
     type: String,
     required: [true, `Please add an author name`]
   },
-  
-})
+  birthdate: {
+    type: Date,
+    required: [true, `Please add an birthdate`]
+  },
+  birthplace: {
+    type: String,
+    maxlength: [255, `Birhplace name can not be more than 255 words`]
+  },
+  education: {
+    university: String,
+    degree: String
+  }
+})  
+
+
+
+
+
+
+
+
+
